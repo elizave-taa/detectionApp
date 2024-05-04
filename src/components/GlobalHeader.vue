e4cbca1ebcaf622fbd85f23e5713ddd496c174c3
elizave-taa <chueva.elizaveta03@gmail.com>
2024-05-04 21:22:02 +0300

    Add global header and assets

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div class="header">
+    <div>
+        <img class="logo" src="../assets/logo.svg">
+    </div>
+    <div class="links">
+        <p>О нас</p>
+        <p>Виды маркировок и значение</p>
+        <p>Контакты</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalHeader',
+  components: {
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+    background-color: rgba(104, 131, 77, 0.55); 
+    backdrop-filter: blur(40px);
+    -webkit-backdrop-filter: blur(40px); 
+    padding: 200px 10px;
+}
+.header{
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello">
-   HELLO WORLD!!!
+    <GlobalHeader/>
+    <ScrollButtonUp/>
   </div>
 </template>
 
 <script>
+import ScrollButtonUp from './ScrollButtonUp.vue'
+import GlobalHeader from './GlobalHeader.vue'
 export default {
   name: 'HelloWorld',
+  components: {
+    ScrollButtonUp, GlobalHeader
+  },
   props: {
     msg: String
   }
@@ -15,18 +21,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
